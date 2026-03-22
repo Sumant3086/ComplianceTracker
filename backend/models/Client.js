5a4835fbe3485cc860eb23cf44177f55e4825f98
sumant3086 <sumantyadav3086@gmail.com>
2026-03-23 04:03:06 +0530

    init: setup backend node project

--- backend/models/Client.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const clientSchema = new mongoose.Schema({
-    company_name: { type: String, required: true },
-    country: { type: String, required: true },
-    entity_type: { type: String, required: true },
-});
-
-module.exports = mongoose.model('Client', clientSchema);
